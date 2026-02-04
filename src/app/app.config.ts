25573ab4acb7fc12e05acb1cd97c23ca385a61ba
Ketlen Dutra <ketlen.dutra44@gmail.com>
2026-02-04 02:49:05 -0300

    dropdown paises configuração inicial

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,9 +3,11 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideRouter, withInMemoryScrolling, withRouterConfig } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimations(),
     provideClientHydration(),
     provideRouter(
       appRoutes,
